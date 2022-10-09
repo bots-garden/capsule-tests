d0970a280ac508ffd968e0fc38897e36f2a86a46
Philippe Charrière <ph.charriere@gmail.com>
2022-10-09 23:52:28 +0000

    📝 save

--- 04-start.sh
+++ 04-start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 docker run \
   -p 8080:8080 \
-  -v $(pwd)/hello:/app --rm k33g/capsule-launcher:0.2.7 \
+  -v $(pwd)/hello:/app --rm k33g/capsule-launcher:0.2.8 \
   /capsule \
   -wasm=./app/hello.wasm \
   -mode=http \
   -httpPort=8080
 
+#gp preview $(gp url 8080)
